4d983f82d2c24ad21c6d9f95900c974034201112
sohansingh2092 <sohansingh@eteaminc.com>
2024-10-19 21:09:39 +0530

    verify otp method

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { login, register, forgotPassword, sendOtp, organizationWithCoaches } = require('../controllers/auth.controller');
+const { login, register, forgotPassword,verifyForgotOtp, sendOtp, organizationWithCoaches } = require('../controllers/auth.controller');
 const { authorizeRole } = require('../middleware/auth.middleware');
 
 const router = express.Router();
@@ -7,6 +7,7 @@ router.post('/login', login);
 router.post('/register', register);
 router.post('/forget-password', forgotPassword);
 router.post('/send-otp',sendOtp);
+router.post('/verify-forget-otp',verifyForgotOtp);
 router.get('/organiz-coach',organizationWithCoaches);
 module.exports = router;
     
